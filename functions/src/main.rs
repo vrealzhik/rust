f4f8ee781682717141a0cada86b9b8f9a0289555
Vlad Zhik <emal@emal.ru>
2024-07-24 18:03:25 +0300

    func

---
+++ functions/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(5);
+    print_labels(5, 'h');
+    let x = plus_one(5);
+    println!("{x}");
+}
+
+fn another_function(x: u32) {
+    println!("Another function. : {x}");
+}
+
+fn print_labels(x: i32, y: char) {
+    println!("Labels: {x} {y}");
+}
+
+fn plus_one() {
+    x + 1
+}
